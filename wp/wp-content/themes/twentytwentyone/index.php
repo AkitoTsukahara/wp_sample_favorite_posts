ababd79d147a1a5228194a3c04fac67eff76d65a
AkitoTsukahara <a-tsukahara@macloud.jp>
2021-12-03 23:26:32 +0900

    pattern3

--- wp/wp-content/themes/twentytwentyone/index.php
+++ wp/wp-content/themes/twentytwentyone/index.php
@@ -21,7 +21,87 @@ get_header(); ?>
 		<h1 class="page-title"><?php single_post_title(); ?></h1>
 	</header><!-- .page-header -->
 <?php endif; ?>
-
+    <div class="post-list">
+        <?php
+            $args = array(
+                'tag' => 'recommend',
+                'posts_per_page' => 3,
+                'orderby' => 'date',
+                'order' => 'ASC'
+            );
+            $query_instance = new WP_Query($args);
+        ?>
+        <?php if ($query_instance->have_posts()): while ($query_instance->have_posts()): $query_instance->the_post(); ?>
+            <div class="post-content">
+                <a href="<?= get_the_permalink();?>">
+                    <div class="post-image" style="background-image: url('<?= get_the_post_thumbnail_url();?>')"></div>
+                </a>
+                <div class="post-data">
+                    <ul class="post-category">
+                        <li>
+                            <a href="/sample"><span><?= get_the_category()[0]->name;?></span></a>
+                        </li>
+                    </ul>
+                    <a href="/sample/1111"><h2><?= get_the_title();?></h2></a>
+                    <p class="date"><?= get_the_date('Y/m/d');?></p>
+                </div>
+            </div>
+        <?php
+        endwhile;
+        endif;
+        wp_reset_postdata();
+        ?>
+    </div>
+    <style>
+        .post-list {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            max-width: 1200px;
+            margin: 0 auto;
+        }
+        .post-content {
+            width: calc(100% / 3 - 18px);
+            margin: 0 18px 18px 0;
+            position: relative;
+            height: 360px;
+            background: #fff;
+        }
+        .post-image {
+            width: 100%;
+            height: 180px;
+            background-position: center;
+            background-size: cover;
+        }
+        .post-data {
+            padding: 30px;
+        }
+        .post-category {
+            font-size: 12px;
+            font-weight: bold;
+        }
+        .post-data h2 {
+            width: 100%;
+            height: 72px;
+            margin-top: 18px;
+            font-size: 18px;
+            font-weight: bold;
+            line-height: 24px;
+        }
+        .post-list ul{
+            list-style: none;
+            margin: 0;
+            padding: 0;
+        }
+        .post-list .date {
+            margin-top: 12px;
+            color: #7e82a6;
+            font-size: 12px;
+        }
+        .post-list a {
+            text-decoration: none;
+        }
+    </style>
 <?php
 if ( have_posts() ) {
 
