025ef54f87dfa4048827911a893f791fda778858
AkitoTsukahara <a-tsukahara@macloud.jp>
2021-12-03 22:19:30 +0900

    pattern2

--- wp/wp-content/themes/twentytwentyone/index.php
+++ wp/wp-content/themes/twentytwentyone/index.php
@@ -21,7 +21,100 @@ get_header(); ?>
 		<h1 class="page-title"><?php single_post_title(); ?></h1>
 	</header><!-- .page-header -->
 <?php endif; ?>
-
+<div class="post-list">
+    <div class="post-content">
+        <a href="#">
+            <div class="post-image" style="background-image: url('/wp-content/uploads/2021/12/dagmara-dombrovska-OLmu1EFnSdk-unsplash.jpg')"></div>
+        </a>
+        <div class="post-data">
+            <ul class="post-category">
+                <li>
+                    <a href="/sample"><span>sample</span></a>
+                </li>
+            </ul>
+            <a href="/sample/1111"><h2>Traditional Christmas Market Begins in Strasbourg, France</h2></a>
+            <p class="date">2021/12/04</p>
+        </div>
+    </div>
+    <div class="post-content">
+        <a href="#">
+            <div class="post-image" style="background-image: url('/wp-content/uploads/2021/12/nick-de-partee-8Vd-WgpK9UQ-unsplash.jpg')"></div>
+        </a>
+        <div class="post-data">
+            <ul class="post-category">
+                <li>
+                    <a href="/sample"><span>sample</span></a>
+                </li>
+            </ul>
+            <a href="/sample/2222"><h2>Christmas tree lighting ceremony in the U.S. capital: famous artists present songs</h2></a>
+            <p class="date">2021/12/04</p>
+        </div>
+    </div>
+    <div class="post-content">
+        <a href="#">
+            <div class="post-image" style="background-image: url('/wp-content/uploads/2021/12/trnava-university-ec-QZhIWbho-unsplash.jpg')"></div>
+        </a>
+        <div class="post-data">
+            <ul class="post-category">
+                <li>
+                    <a href="/sample"><span>sample</span></a>
+                </li>
+            </ul>
+            <a href="/sample/3333"><h2>10 recommendations for stylish postcards. From Scandinavian designs to cute illustrations</h2></a>
+            <p class="date">2021/12/04</p>
+        </div>
+    </div>
+</div>
+<style>
+    .post-list {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        max-width: 1200px;
+        margin: 0 auto;
+    }
+    .post-content {
+        width: calc(100% / 3 - 18px);
+        margin: 0 18px 18px 0;
+        position: relative;
+        height: 360px;
+        background: #fff;
+    }
+    .post-image {
+        width: 100%;
+        height: 180px;
+        background-position: center;
+        background-size: cover;
+    }
+    .post-data {
+        padding: 30px;
+    }
+    .post-category {
+        font-size: 12px;
+        font-weight: bold;
+    }
+    .post-data h2 {
+        width: 100%;
+        height: 72px;
+        margin-top: 18px;
+        font-size: 18px;
+        font-weight: bold;
+        line-height: 24px;
+    }
+    .post-list ul{
+        list-style: none;
+        margin: 0;
+        padding: 0;
+    }
+    .post-list .date {
+        margin-top: 12px;
+        color: #7e82a6;
+        font-size: 12px;
+    }
+    .post-list a {
+        text-decoration: none;
+    }
+</style>
 <?php
 if ( have_posts() ) {
 
